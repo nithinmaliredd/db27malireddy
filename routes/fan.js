f4a8b39f19c79a5e185761b4aea0ce89a501fe49
nithinmaliredd <s544927@nwmissouri.edu>
2021-11-29 20:00:20 -0600

    updated

--- routes/fan.js
+++ routes/fan.js
@@ -1,7 +1,16 @@
 var express = require('express');
 const fan_controlers= require('../controllers/fan');
 var router = express.Router();
-
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
 
 /* GET costumes */
 router.get('/', fan_controlers.fan_view_all_Page);
@@ -12,7 +21,7 @@ router.get('/', fan_controlers.fan_view_all_Page);
 router.get('/create', fan_controlers.fan_create_Page);
 
 
-router.get('/update', fan_controlers.fan_update_Page); 
+router.get('/update', secured, fan_controlers.fan_update_Page); 
 
 router.get('/delete', fan_controlers.fan_delete_Page); 
 
